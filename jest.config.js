53bcb79827c97a66438fffa49c2595958e0d14de
pcktbot <pcktbot@gmail.com>
2023-12-13 13:05:06 -0600

    configs

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 export default {
-  roots: ['<rootDir>/src'],
-  transform: {},
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testEnvironment: 'jsdom',
+  transform: {},
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
+  extensionsToTreatAsEsm: ['.ts'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
